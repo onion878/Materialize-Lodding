46aa8e1091e6e7825310a571aab23bb5b53cc66a
Onion <a2214839296a@gmail.com>
2018-03-09 11:33:23 +0800

    first commit

---
+++ js/plugins.js
@@ -0,0 +1,19 @@
+/*================================================================================
+  Item Name: Materialize - Material Design Admin Template
+  Version: 1.0
+  Author: GeeksLabs
+  Author URL: http://www.themeforest.net/user/geekslabs
+================================================================================*/
+
+$(function() {
+
+  "use strict";
+
+  $(window).load(function() {
+    setTimeout(function() {
+      $('body').addClass('loaded');      
+    }, 500);
+  });  
+  
+
+}); // end of document ready
